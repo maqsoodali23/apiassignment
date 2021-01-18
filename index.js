66d521602f825b305f86cc7689d8f1cd27a71e98
Maqsood Shah <maqsood.ali@invozone.com>
2021-01-18 16:09:06 +0500

    Initial code commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+app.get('/', (req, res) => {
+    res.send(`Welcome to home page`);
+});
+app.listen(port, () => {
+    console.log(`Appis listening to port ${port}`);
+});
